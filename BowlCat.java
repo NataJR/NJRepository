db18527c6fdd6d9984d178093c9941db1d45b89a
NataJR <55583096+NataJR@users.noreply.github.com>
2019-12-27 03:16:52 +0100

    Add files via upload

---
+++ BowlCat.java
@@ -0,0 +1,5 @@
+public class BowlCat extends Bowl {
+    BowlCat(int food) {
+        super(food);
+    }
+}
---
+++ CatApp.java
@@ -0,0 +1,5 @@
+public class CatApp extends Cat {
+    public CatApp(String modest, int i, int i1) {
+        super(modest, i, i1);
+    }
+}
